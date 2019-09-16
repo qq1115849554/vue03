e8c170d0f77d8d1e2cf501f78becd9f85674d5bb
yzx <1115849554@qq.com>
2019-09-16 09:53:49 +0800

    vue学习 2019/9/12 组件化开发

---
+++ v-modelLearn/main.js
@@ -0,0 +1,15 @@
+new Vue({
+    el:"#app",
+    data:{
+        message:'请输入用户名：',
+        message2:0,
+        sex:'',
+        isAgree:false,
+        myRace:[],
+        fruit:'极地',
+        fruits:[],
+        races:['极地','暗影','忍者','帝国','狂野'],
+        myRaces:[]
+
+    }
+})
